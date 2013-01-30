cba19fa3340d2f8fa9147323fe2abc15522253ba
Phil Bayfield <phil@bayfmail.com>
2013-01-30 06:18:06 +0100

    classmap autoloader update

--- libs/LiteMVC/Autoloader/Classmap.php
+++ libs/LiteMVC/Autoloader/Classmap.php
@@ -3,7 +3,7 @@
 /**
  * LiteMVC Application Framework
  * 
- * MySQL driver for ORM
+ * Classmap autoloader
  *
  * @author Phil Bayfield
  * @copyright 2010 - 2013
@@ -15,7 +15,7 @@
 namespace LiteMVC\Autoloader;
 
 class Classmap {
-    
+
     /**
      * Map of all framework classes
      *
@@ -34,22 +34,40 @@ class Classmap {
         'LiteMVC\View\AbstractView' => '/View/AbstractView.php',
         'LiteMVC\View\Exception' => '/View/Exception.php',
     );
-    
+
     /**
      * Root director for the framework files
      *
      * @var string 
      */
     protected $_root;
-    
+
     /**
      * Set the root director and register the autoloader
+     * 
+     * @param boolean $autoRegister
      */
-    public function __construct() {
+    public function __construct($autoRegister = true) {
         $this->_root = realpath(__DIR__ . '/../');
+        if ($autoRegister) {
+            $this->register();
+        }
+    }
+
+    /**
+     * Register the autoloader
+     */
+    public function register() {
         spl_autoload_register(array($this, 'load'));
     }
     
+    /**
+     * Unregister the autoloader
+     */
+    public function unregister() {
+        spl_autoload_unregister(array($this, 'load'));
+    }
+
     /**
      * Attempt to autoload a class
      * 
@@ -60,5 +78,5 @@ class Classmap {
             require_once $this->_root . $this->_map[$class];
         }
     }
-    
+
 }

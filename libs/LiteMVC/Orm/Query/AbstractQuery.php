9d9cc18ee8ed88675c059dce77bd05c741a6ce05
Phil Bayfield <phil@bayfmail.com>
2014-03-16 03:08:10 +0000

    add initial work on simple query builder

---
+++ libs/LiteMVC/Orm/Query/AbstractQuery.php
@@ -0,0 +1,231 @@
+<?php
+
+/**
+ * LiteMVC Application Framework
+ *
+ * Abstract query builder for Orm
+ *
+ * @author Phil Bayfield
+ * @copyright 2010 - 2013
+ * @license GNU General Public License version 3
+ * @package LiteMVC
+ * @version 0.4.0
+ */
+
+namespace LiteMVC\Orm\Query;
+
+abstract class AbstractQuery
+{
+
+    /**
+     * Separator
+     *
+     * @var string
+     */
+    const SEPARATOR = '.';
+
+    /**
+     * Param
+     *
+     * @var string
+     */
+    const PARAM = '?';
+
+    /**
+     * Tables used in query
+     *
+     * @var array
+     */
+    protected $_tables = array();
+
+    /**
+     * Columns used in query
+     *
+     * @var array
+     */
+    protected $_columns = array();
+
+    /**
+     * Join statements
+     *
+     * @var array
+     */
+    protected $_joins = array();
+
+    /**
+     * Where statement
+     *
+     * @var string
+     */
+    protected $_where;
+
+    /**
+     * Bound params
+     *
+     * @var array
+     */
+    protected $_params = array();
+
+    /**
+     * Add a table to the query
+     *
+     * @param string $table
+     * @param string|null $identifier
+     * @return AbstractQuery
+     */
+    public function addTable($table, $identifier = null)
+    {
+        if (is_null($identifier)) {
+            $this->_tables[] = $table;
+        } else {
+            $this->_tables[$identifier] = $table;
+        }
+        return $this;
+    }
+
+    /**
+     * Add columns to the query
+     *
+     * @param string $column
+     * @param string|null $table
+     * @param string|null $identifier
+     * @return AbstractQuery
+     * @throws Exception
+     */
+    public function addColumn($column, $table = null, $identifier = null)
+    {
+        // Require a valid table name or identifier
+        if (!array_key_exists($table, $this->_tables) && !in_array($table, $this->_tables)) {
+            throw new Exception('Table name or identifier unknown');
+        }
+
+        // Prefix table name
+        if (!is_null($table)) {
+            $column = $table . self::SEPARATOR . $column;
+        }
+
+        if (is_null($identifier)) {
+            $this->_columns[] = $column;
+        } else {
+            $this->_columns[$identifier] = $column;
+        }
+        return $this;
+    }
+
+    /**
+     * Add where clause to query
+     *
+     * @param string $where
+     * @param array $params
+     * @return AbstractQuery
+     * @throws Exception
+     */
+    public function where($where, array $params = null)
+    {
+        // Basic WHERE verification
+        if (strstr($where, self::PARAM) !== false) {
+            if ($params == null) {
+                throw new Exception('Where statement contains bindings but no params given');
+            }
+            if (substr_count($where, self::PARAM) != count($params)) {
+                throw new Exception('Number of params should match number of where bindings');
+            }
+        }
+
+        $this->_where = $where;
+        $this->_params = $params;
+        return $this;
+    }
+
+    /**
+     * Allow casting to string
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->buildQuery();
+    }
+
+    /**
+     * Children should implement to build SQL
+     *
+     * @return string
+     */
+    abstract public function buildQuery();
+
+    /**
+     * Get params that should be bound to this query
+     *
+     * @return array
+     */
+    public function getQueryParams()
+    {
+        return $this->_params;
+    }
+
+    /**
+     * Helper method to create list of tables in SQL format
+     *
+     * @return string
+     */
+    protected function _buildTableList()
+    {
+        // Get last table key
+        end($this->_tables);
+        $lastKey = key($this->_tables);
+        reset($this->_tables);
+
+        // Add tables to list
+        $tableList = '';
+        foreach ($this->_tables as $identifier => $table) {
+            $tableList .= $table;
+            if (!is_numeric($identifier)) {
+                $tableList .= ' AS ' . $identifier;
+            }
+            if ($identifier != $lastKey) {
+                $tableList .= ', ';
+            }
+        }
+        return $tableList;
+    }
+
+    /**
+     * Helper method to create list of columns in SQL format
+     *
+     * @return string
+     */
+    protected function _buildColumnList()
+    {
+        // Get last column key
+        end($this->_columns);
+        $lastKey = key($this->_columns);
+        reset($this->_columns);
+
+        // Add columns to query
+        $column = '';
+        foreach ($this->_columns as $identifier => $column) {
+            $column .= $column;
+            if (!is_numeric($identifier)) {
+                $column .= ' AS ' . $identifier;
+            }
+            if ($identifier != $lastKey) {
+                $column .= ', ';
+            }
+        }
+        return $column;
+    }
+
+    /**
+     * Helper method to build where statement
+     *
+     * @return string
+     */
+    protected function _buildWhere()
+    {
+        if ($this->_where != null) {
+            return 'WHERE ' . $this->_where;
+        }
+    }
+
+} 
---
+++ libs/LiteMVC/Orm/Query/Select.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * LiteMVC Application Framework
+ *
+ * Select builder for Orm
+ *
+ * @author Phil Bayfield
+ * @copyright 2010 - 2013
+ * @license GNU General Public License version 3
+ * @package LiteMVC
+ * @version 0.4.0
+ */
+
+namespace LiteMVC\Orm\Query;
+
+class Select extends AbstractQuery
+{
+
+    /**
+     * Add tables to query
+     *
+     * Example usage:
+     *
+     * from('table_1')
+     * from(array('t1' => 'table_1'))
+     * from('table_1', 'table_2')
+     * from(array('t1' => 'table_1', 't2' => 'table_2'))
+     *
+     * @param mixed $table,...
+     * @return Select
+     */
+    public function from($table)
+    {
+        // Multiple args accepted as an array
+        if (func_num_args() > 1) {
+            $table = func_get_args();
+        }
+
+        // Iterate array or add string
+        if (is_array($table)) {
+            foreach ($table as $identifier => $table) {
+                $this->addTable($table, !is_numeric($identifier) ? $identifier : null);
+            }
+        } else {
+            $this->addTable($table);
+        }
+        return $this;
+    }
+
+    /**
+     * Add columns to query
+     *
+     * Example usage:
+     *
+     * columns('col_1', 'col_2', 'col_3');
+     * columns(array('t1' => array('col_1', 'col_2', 'col_3')))
+     * columns(array('t1' => array('c1' => 'col_1', 'c2' => 'col_2', 'c3' => 'col_3')))
+     *
+     * @param mixed $columns,...
+     * @return Select
+     * @throws Exception
+     */
+    public function columns($columns)
+    {
+        // Multiple args accepted as an array
+        if (func_num_args() > 1) {
+            $columns = func_get_args();
+        }
+
+        // Iterate array or add string
+        if (is_array($columns)) {
+            foreach ($columns as $key => $value) {
+                if (is_array($value)) {
+                    // Require a table name or identifier as the array key
+                    if (is_numeric($key)) {
+                        throw new Exception('Table name or identifier expected');
+                    }
+
+                    // Add all columns from array
+                    foreach ($value as $identifier => $column) {
+                        $this->addColumn($column, $key, !is_numeric($identifier) ? $identifier : null);
+                    }
+                } else {
+                    $this->addColumn($value, !is_numeric($key) ? $key : null);
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Generate SQL
+     *
+     * @return string
+     */
+    public function buildQuery()
+    {
+        return sprintf('SELECT %s FROM %s %s', $this->_buildColumnList(), $this->_buildTableList(), $this->_buildWhere());
+    }
+
+} 

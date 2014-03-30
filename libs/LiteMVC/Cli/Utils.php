6f78543577bf477199bf6d547a56ad90b2646272
Phil Bayfield <phil@bayfmail.com>
2014-03-30 09:32:29 +0100

    fix coding style violations

--- libs/LiteMVC/Cli/Utils.php
+++ libs/LiteMVC/Cli/Utils.php
@@ -37,21 +37,37 @@ class Utils
      * @var string
      */
     const COLOR_BLACK = "\033[0;30m";
+
     const COLOR_DARK_GRAY = "\033[1;30m";
+
     const COLOR_RED = "\033[0;31m";
+
     const COLOR_LIGHT_RED = "\033[1;31m";
+
     const COLOR_GREEN = "\033[0;32m";
+
     const COLOR_LIGHT_GREEN = "\033[1;32m";
+
     const COLOR_BROWN = "\033[0;33m";
+
     const COLOR_YELLOW = "\033[1;33m";
+
     const COLOR_BLUE = "\033[0;34m";
+
     const COLOR_LIGHT_BLUE = "\033[1;34m";
+
     const COLOR_PURPLE = "\033[0;35m";
+
     const COLOR_LIGHT_PURPLE = "\033[1;35m";
+
     const COLOR_CYAN = "\033[0;36m";
+
     const COLOR_LIGHT_CYAN = "\033[1;36m";
+
     const COLOR_LIGHT_GREY = "\033[0;37m";
+
     const COLOR_WHITE = "\033[1;37m";
+
     const COLOR_TERMINATOR = "\033[0m";
 
     /**
@@ -78,8 +94,7 @@ class Utils
      * @param array $optParams
      */
     public static function showHelpEntry($moduleName, $actionName, $desc = null, array $reqParams = array(),
-        array $optParams = array())
-    {
+        array $optParams = array()) {
         // Merge all required params
         $req = array_merge(array(self::CMD, $moduleName, $actionName), $reqParams);
 
